2ec296cd8a77136828f7b03254090686db8915b0
Martin Delille <martin@phonations.com>
2014-02-20 23:36:54 +0100

    test gui testing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include "QtTestWindow.h"
 #include <QApplication>
+#include <QThread>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+	bool testMode = false;
+
+	for(int i = 1; i < argc; i++) {
+		if(strcmp(argv[i], "test") == 0)
+			testMode = true;
+	}
 	QApplication a(argc, argv);
 	QtTestWindow w;
 	w.show();
 
-	return a.exec();
+	if(testMode) {
+		qDebug() << "Entering test mode";
+
+		QThread::msleep(1000);
+		qDebug() << "One";
+		QThread::msleep(1000);
+		qDebug() << "Two";
+		QThread::msleep(1000);
+		qDebug() << "Three";
+		QThread::msleep(1000);
+		qDebug() << "Bye";
+		return 0;
+	}
+	else
+		return a.exec();
 }
