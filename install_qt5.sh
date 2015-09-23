b3af1de4ec63c0d4d28fe07695ab7f188880ca3b
Martin Delille <martin@phonations.com>
2015-09-23 17:56:12 +0200

    test beineri ppa

--- install_qt5.sh
+++ install_qt5.sh
@@ -6,9 +6,9 @@ brew install qt5
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo add-apt-repository --yes ppa:beineri/opt-qt541
 sudo apt-get update -qq
-sudo apt-get install -qq qt5-qmake qtbase5-dev qtdeclarative5-dev
+sudo apt-get install -qq qt54base qt54xmlpatterns
 #sudo apt-get install -qq libqt5webkit5-dev libsqlite3-dev
 fi
 
