f7ff240eefa3cb89038a3f02ffd35d502b7ae5e2
Martin Delille <martin@phonations.com>
2016-01-16 12:51:58 +0100

    echo COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN

--- coverity.sh
+++ coverity.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
 echo "JOB: $JOB"
+echo "COVERITY_SCAN_PROJECT_NAME: $COVERITY_SCAN_PROJECT_NAME"
+echo "COVERITY_SCAN_TOKEN: $COVERITY_SCAN_TOKEN"
 
 if [[ $TRAVIS_OS_NAME != osx || $JOB != AAA ]]; then
   echo "Bad build configuration: $TRAVIS_OS_NAME / $JOB"
